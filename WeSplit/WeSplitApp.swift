3aea06d6b31699625aa11b5d39140fe91b530451
ashusharma7 <ashu7073657271@gmail.com>
2023-02-12 21:27:23 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by ashu sharma on 12/02/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
